c4c669800ce0735e62fefad502738be0d6fcfb9f
Harishankar S <67646220+Hurry-Nair@users.noreply.github.com>
2021-03-03 09:13:43 +0530

    q7 - q17

--- pythonQuiz.py
+++ pythonQuiz.py
@@ -58,7 +58,7 @@ def q6():
 		print('no')
 	# This can not be true for any value of range(n)
 	# no
-	
+
 def q7():
 	class A:
 		def __init__(self, x):
@@ -70,7 +70,10 @@ def q7():
 	a = A(2)
 	b = A(2)
 	print(a == b)
+	# a.__eq__(self, b)
+	# True
 	print(a is b)
+	# is compared memory address of objects 
 	print(b is a)
 	print(a is a)
 	print(a == a)
@@ -94,12 +97,32 @@ def q8():
 			self.z = z
 
 	d = A(2, 4)
+	# d.x = 2
+	# d.y = 4
+	
 	e = B(4, 5)
+	# Uses init of parent class
+	# Uses print of its own
+	# e.x = 4
+	# e.y = 5
+
 	g = C(3, 4, 7)
+	# Utilizes init of parent class &
+	# modifies it for 3rd variable
+	# simply inherits print from parent (B)
+	# g.x = 3
+	# g.y = 4
+	# g.z = 7
+	
 	g.x = g.z
+	# g.x = 7
+	
 	d.print()
+	# 2 4
 	e.print()
+	# 5 4
 	g.print()
+	# 4 7
 
 def q9():
 	def x(z):
@@ -111,8 +134,23 @@ def q9():
 
 	for i in range(10):
 		func = x(i)
+		# func = q at this point with z = i
 		func(i, i-1)
+		# th
+'''
+i = 0
+x = 0, y = -1
+>> -1
 
+i = 1
+x = 1, y = 0
+>> 2
+
+i = 2
+x = 2, y = 1
+>> 5
+
+''' 
 def q10():
 	def d(f):
 		def w(*args, **kwargs):
@@ -127,42 +165,54 @@ def q10():
 		return x + 1
 
 	print(a(5))
+# Control flow
+# print invokes a
+# func a gets passed as f into d since d decorates a
+# d tries to return w & invokes it
+# w runs f & initiaites & mofies r
+# r is returned
 
 def q11():
 	print(*list(map(lambda x: chr(ord(x) + 1), ["a", "b", "c"])))
-
+# map() maps arguments to the function
+# lambda arguments : expression
+# * unpacks list into individual variables
 
 def q12():
 	x = 0.1
 	y = 0.10000000000000001
 	print(x == y)
+# True
 
 def q13():
 	x = [True, 1, "a", "b", "2"]
 	print(any(x)) 
+# True
 
 def q14():
 	x = ["a", 1, 2, 3, 4]
-	y = z = x
-	z[1] = 7
-	y[3] = 2
-	x[2] = 9
-	print(x)
-	print(y)
-	print(z)
+	y = z = x # shallow copy
+	z[1] = 7 # ["a", 7, 2, 3, 4]
+	y[3] = 2 # ["a", 7, 2, 2, 4]
+	x[2] = 9 # ["a", 7, 9, 2, 4]
+	print(x) # ["a", 7, 9, 2, 4]
+	print(y) # ["a", 7, 9, 2, 4]
+	print(z) # ["a", 7, 9, 2, 4]
 
 def q15():
-	print(1 == True)
-	print("1" == 1)
+	print(1 == True) # True
+	print("1" == 1) # False
 
 def q16():
 	x = b'1001'
 	y = b'1010'
 	z = x + y
 	print(z)
+	# b'10011010'
 
 def q17():
-	x = 0b1001
-	y = 0b1010
-	z = x + y
+	x = 0b1001 # 9
+	y = 0b1010 # 10
+	z = x + y # 19
 	print(z)
+
